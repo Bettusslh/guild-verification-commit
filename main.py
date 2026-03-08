44f720350aaac6d94c84045075427c9703136fcf
Bettusslh <bettusslh@hotmail.com>
2026-03-08 07:03:57 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T04:03:45.261Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T04:03:58.336Z
+Commit: Resolve null pointer exception in user service
