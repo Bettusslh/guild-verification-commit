305e518e0e3d1f6060818aa8d035e9482cc2b965
Bettusslh <bettusslh@hotmail.com>
2026-03-08 07:06:51 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T04:06:47.420Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-08T04:06:51.419Z
+Commit: Fix broken links in documentation
