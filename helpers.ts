a1cbec16efc54c4dee678745dc4490063e47bd5e
Bettusslh <bettusslh@hotmail.com>
2026-03-08 07:04:06 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T04:04:01.102Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-08T04:04:07.115Z
+Commit: Patch security vulnerability in authentication
