48142d1e40dbedb76c5ec0f9b0db4cd27b32ae96
Bettusslh <bettusslh@hotmail.com>
2026-03-08 07:05:03 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T04:04:58.372Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T04:05:03.587Z
+Commit: Fix CSS styling bug on mobile devices
