aa957aab29d82235499e71cd3a78a71d20f92a66
Bettusslh <bettusslh@hotmail.com>
2026-03-08 07:06:09 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T04:05:43.842Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T04:06:09.422Z
+Commit: Fix memory leak in event handlers
