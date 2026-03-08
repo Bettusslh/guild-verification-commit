bc5e94890ac7bbd9eb7138a029554edc256df984
Bettusslh <bettusslh@hotmail.com>
2026-03-08 07:05:18 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T04:05:14.999Z
-Commit: Add dark mode support
+Timestamp: 2026-03-08T04:05:18.738Z
+Commit: Resolve null pointer exception in user service
