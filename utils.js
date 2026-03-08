b1da023e06ede887f783fccb7ea7ac9450813b8e
Bettusslh <bettusslh@hotmail.com>
2026-03-08 07:06:02 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T04:05:59.919Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T04:06:02.424Z
+Commit: Fix broken links in documentation
